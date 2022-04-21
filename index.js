2c455ad1337436fdc16bc8a080d08b7db713db37
Shokun tube <shokun220649@gmail.com>
2022-04-21 12:30:12 +0700

    v.2

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateimage = require("./generateimage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
 client.on("ready", () => {
-    console.log('Logged in as ${client.user.tag}')
+    console.log(`Logged in as ${client.user.tag}`)
 })
 
 client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("Hello!")
     }
+})
+
+const welcomeidchannel = "966249182014873631"
+
+client.on("guildMemberAdd", async (member) =>{
+    const img = await generateimage(member)
+    member.guild.channels.cache.get(welcomeidchannel).send({
+        content: `<@${member.id}> Welcome to the server`,
+        files: [img]
+    })
 })
 
 client.login(process.env.token)
